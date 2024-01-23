c3b67b7786537cfe517d0bfefec8aa90bf466132
diegohansselperez <diegohansselp24@gmail.com>
2024-01-22 23:10:02 -0700

    Navbar comenzado a un 50 de su totalidad

--- src/scenes/Navbar.jsx
+++ src/scenes/Navbar.jsx
@@ -10,7 +10,22 @@ const Navbar = () => {
   return (
     <nav>
       <div className={`${flexBetween} fixed top-0 z-30 w-full py-6`}>
-        <div></div>
+        <section className={`${flexBetween} mx-auto w-5/6 `}>
+          {' '}
+          <img src={Logo} alt="logo evogym" />
+          <div className={`${flexBetween} w-full gap-16 `}>
+            <div className={`${flexBetween} w-full gap-8 `}>
+              <p>Home</p>
+              <p>Benefits</p>
+              <p>Our Classes</p>
+              <p>Contact Us</p>
+            </div>
+            <div className={`${flexBetween} gap-8 `}>
+              <p>Sing In</p>
+              <button>Become a Member</button>
+            </div>
+          </div>
+        </section>
       </div>
     </nav>
   );
